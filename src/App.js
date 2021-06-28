bf0bf2679f7a2fa421abe6b10303654a61e8d97c
Jubayer Juhan <davidjuhan23@gmail.com>
2021-06-28 14:19:00 +0600

    Just Added The Name And Flags

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+const [Country, setCountry] = useState([]);
+
+useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => setCountry(data))
+    .catch(error => console.log(error))
+}, [])
+console.log(Country)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div>
+        {Country.map((countries) => 
+          <div className="nameAndFlag">
+            <h2 className= "countriesName">{countries.name}</h2>
+            <img class="flags" src={countries.flag} alt="" />
+          </div>
+        )}
+      </div>
+
     </div>
   );
 }
